ef67c0353c2cc5c04d4ea66fef19f694f52ac358
Mykyta Krumalenko <prikol769@gmail.com>
2024-07-03 12:48:29 -0500

    added getDogs

---
+++ requestsApi.js
@@ -0,0 +1,24 @@
+const API_KEY =
+  "live_Xp7ikvHiiSV8XDllZLmCfDgTcMkiU7iHUsITcfH6iFEePdHwWCYHEGDb9wtF0Xty";
+
+axios.defaults.baseURL = "https://api.thedogapi.com/v1";
+axios.defaults.headers.common["x-api-key"] = API_KEY;
+
+let dogsArr = [];
+let reqLimit = 0;
+
+export const getDogs = async (limit) => {
+  reqLimit += limit;
+  try {
+    const response = await axios(`/breeds?limit=${reqLimit}`);
+    const dogsData = response.data;
+
+    const lastTenDogs = dogsData.slice(Math.max(dogsData.length - limit, 0));
+
+    dogsArr.push(...lastTenDogs);
+
+    return dogsArr.slice(Math.max(dogsData.length - limit, 0));
+  } catch (error) {
+    console.log(error);
+  }
+};
